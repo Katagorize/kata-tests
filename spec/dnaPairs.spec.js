3711caaafea12f3f1c4c0cbaecedcc98f4a8a069
Johnny Dunsby <jmdunsby@gmail.com>
2018-02-02 10:49:42 +0000

    add tests for dnaPairs

--- spec/dnaPairs.spec.js
+++ spec/dnaPairs.spec.js
@@ -25,4 +25,20 @@ describe('dnaPairs()', function () {
     expect(dnaPairs('SA')).to.eql([['A','T']]);
     expect(dnaPairs('SAhyTgGcC')).to.eql([['A','T'],['T','A'],['G','C'],['G','C'],['C','G'],['C','G']]);
   });
+  it('Returns an array of matched pairs when passed valid a string of characters with spaces', () => {
+    expect(dnaPairs('SA h yT gG cC')).to.eql([['A','T'],['T','A'],['G','C'],['G','C'],['C','G'],['C','G']]);    
+  });
+  it('Returns an empty array when passed a string of non-base characters', () => {
+    expect(dnaPairs('4567890')).to.eql([]);
+    expect(dnaPairs('QQQQQXZZZ')).to.eql([]);
+    expect(dnaPairs('Sh2yGf2vbn6N')).to.eql([]);
+  });
+  it('Returns an empty array when passed an invalid input', () => {
+    expect(dnaPairs([])).to.eql([]);
+    expect(dnaPairs({})).to.eql([]);
+    expect(dnaPairs(0)).to.eql([]);
+    expect(dnaPairs(true)).to.eql([]);
+    expect(dnaPairs(false)).to.eql([]);
+    expect(dnaPairs(null)).to.eql([]);
+  });
 });
